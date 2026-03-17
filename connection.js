7894c7d31d207dc3c4e286d13faf5f5c00030ec4
Pushpendra-singh0866 <pushpendrasingh0241123@gmail.com>
2026-03-17 22:40:26 +0530

    database

---
+++ connection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+//const url = 'mongodb+srv://Pushpendrasingh:Pushpa2005@cluster0.x2ji9jn.mongodb.net/mydb?appName=Cluster0';
+
+const url='mongodb://Pushpendrasingh:Pushpa2005@ac-3ognvsn-shard-00-00.x2ji9jn.mongodb.net:27017,ac-3ognvsn-shard-00-01.x2ji9jn.mongodb.net:27017,ac-3ognvsn-shard-00-02.x2ji9jn.mongodb.net:27017/?ssl=true&replicaSet=atlas-vzjsif-shard-0&authSource=admin&appName=Cluster0'
+mongoose.connect(url)
+.then((result) => {
+    console.log('database connected');
+        
+})
+.catch((err) => {
+    console.log(err);
+});
+
+
+module.exports=mongoose;
---
+++ models/userModel.js
@@ -0,0 +1,9 @@
+const {Schema,model} = require('../connection');
+
+const mySchema=new Schema({
+    name:{type:String,require:true},
+    email:{type:String,require:true},
+    password:{type:String,require:true},
+},{timestamps:true});
+
+module.exports=model('user',mySchema);
